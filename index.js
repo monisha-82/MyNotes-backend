06d794d1716113827d8d23a90cced5826cb1a007
Monisha Choudhury <112749649+monisha-82@users.noreply.github.com>
2024-12-06 23:13:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express()
 // To make input as json
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({origin: ["*"], credentials: true}))
+app.use(cors({origin: ["http://localhost:5173"], credentials: true}))
 
 const PORT = process.env.PORT || 3000
 
